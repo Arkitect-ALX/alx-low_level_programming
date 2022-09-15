5c5ffa496ecad22a27d9420042ebb2a5bb2df1b6
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-14 20:28:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * Task:    0._Putchar
+ * Author:  Aruoture Ivwighren
+ *
+ *
+ * Main:    Writre a program that prints _putchar, followed by a newline.
+ * Return:  Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
