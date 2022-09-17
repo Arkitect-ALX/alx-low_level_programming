1ebb52228f5a29193c1edc952fe83a14a4c51a2f
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-17 08:21:54 -0700

    print numbers except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print numbers 0 - 9 except 2 & 4
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if ((i == 50) || (i == 52))
+		{
+			continue;
+		}
+		_putchar(n);
+	}
+	_putchar('\n');
+}
