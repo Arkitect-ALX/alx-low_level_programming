5e27b43cbed3aeca4220c677528fc817eedd14e0
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-17 08:01:33 -0700

    printing 0 - 9 continously

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Functions prints digits from 0 - 9
+ *
+ * Return:
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
