81f0d069006794532306bfa03e33c069e25aa677
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-17 09:39:23 -0700

    Readjused task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - Functions prints digits from 0 - 9
  *
- * Return:
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(n);
-	}
-	putchar(10);
+	for (i = 0; i < 10; i++)
+	
+		_putchar(i + '0');
+	
+	_putchar('\n');
 }
