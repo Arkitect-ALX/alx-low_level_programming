0212bfbcdb15b7a6e57f3b3279070fd678193105
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-17 00:05:26 -0700

    Adjusted the return value for the n > 0 condition

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,14 +2,14 @@
 /**
  * print_sign - this fucntion will display  the sign of a number
  * @n: is the int valye for our function argument
- * Return: 0  in success
+ * Return: 1, 0. -1 depending on condition
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 	_putchar('+');
-	return (0);
+	return (1);
 }
 	else if (n < 0)
 	{
