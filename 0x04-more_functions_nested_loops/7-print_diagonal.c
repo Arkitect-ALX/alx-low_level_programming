6fb267f1a5bd030f7b071134771d23271937da01
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-18 04:25:05 -0700

    Hot Fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int j, k;
+	int x, i;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (j = 0; j < n; j++)
+		for (x = 0; x < n; x++)
 		{
-			for (k = 0; k <= j; k++)
+			for (i = 0; i <= x; i++)
+
 				_putchar(' ');
 				_putchar(92);
 				_putchar('\n');
