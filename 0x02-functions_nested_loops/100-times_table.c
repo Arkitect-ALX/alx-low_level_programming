aea8a15acdd6babb97b5fd9457384e2b8f2f3b7d
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-19 16:04:20 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,52 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n tables, counting starts from 0
- * @n: number of he times table
+ * print_times_table - prints the n times table starting with 0 between 0-15
+ * @n: Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int r;
+	int i, j, k;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				r = i * j;
-				print_any_int(r);
-
-				if (j < n)
+				k = i * j;
+				if (j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					if ((i * (j + 1)) < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if ((i * (j + 1)) < 100)
-					{
-						_putchar(' ');
-					}
+					_putchar (',');
+					_putchar (' ');
+				if (k >= 10 && k < 100)
+				{
+					_putchar (' ');
+					_putchar (k / 10 + '0');
+					_putchar (k % 10 + '0');
+				}
+				else if (k < 10)
+				{
+					_putchar (' ');
+					_putchar (' ');
+					_putchar (k + '0');
+				}
+				else
+				{
+					_putchar (k / 100 + '0');
+					_putchar ((k / 10) % 10 + '0');
+					_putchar (k % 10 + '0');
 				}
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar (k  + '0');
+			}
+			}
+		_putchar('\n');
 		}
 	}
 }
