c65011c276e04840ceabcf2ee15226fd88adc75a
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-19 13:47:38 -0700

    Print straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,20 @@
 #include "main.h"
+
 /**
- * print_line - print a steight line to screen continously
- * @n: variable to be passed as argument
- *
+ * print_line -  Function to print a straight line
+ * @n: n -  Variable
+ * Return: Always 0.
  */
 void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x < n; x++)
-		_putchar(95);
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar(95);
+		}
+	}
 	_putchar('\n');
 }
