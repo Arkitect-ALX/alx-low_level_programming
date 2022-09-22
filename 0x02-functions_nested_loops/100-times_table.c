cdf803bc6aec47b53a90239b3d4016455bbaa93d
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-22 12:56:23 -0700

    We het tired sometime

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,48 @@
 #include "main.h"
-
 /**
- * print_times_table - prints the n times table starting with 0 between 0-15
- * @n: Integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int i, j, k;
 
-	if (n >= 0 && n < 15)
+int i, j, res;
+
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
+				res = (i * j);
 				if (j != 0)
 				{
-					_putchar (',');
-					_putchar (' ');
-				if (k >= 10 && k < 100)
-				{
-					_putchar (' ');
-					_putchar (k / 10 + '0');
-					_putchar (k % 10 + '0');
+					putchar(',');
+					putchar(' ');
 				}
-				else if (k < 10)
+				if (res < 10 && j != 0)
 				{
-					_putchar (' ');
-					_putchar (' ');
-					_putchar (k + '0');
+					putchar(' ');
+					putchar(' ');
+					putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					putchar(' ');
+					putchar((res / 10) + '0');
+					putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					putchar((res / 100) + '0');
+					putchar((res / 10) % 10 + '0');
+					putchar((res % 10) + '0');
 				}
 				else
-				{
-					_putchar (k / 100 + '0');
-					_putchar ((k / 10) % 10 + '0');
-					_putchar (k % 10 + '0');
-				}
+					putchar((res % 10) + '0');
 			}
-			else
-			{
-				_putchar (k  + '0');
-			}
-			}
-		_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
