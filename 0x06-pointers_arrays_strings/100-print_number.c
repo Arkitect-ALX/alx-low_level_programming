111d9e53b93957356801a3a9e0b595ac6ab2f7a2
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-23 07:47:07 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,49 +1,23 @@
 #include "main.h"
+
 /**
- * print_number - print integer
- * Return: void
- * @n: number
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int size = n;
-
-	int digits = 0;
-
-	int i;
-
+	unsigned int num = n;
 	if (n < 0)
-		size = -n;
-	if (n == 0)
-		digits = 1;
-
-	while (size >= 1)
+	
 	{
-		size = size / 10;
-		digits++;
+		_putchar('-');
+		
+		num = -num;
 	}
-
-	for (i = 0; i < digits; i++)
-	{
-
-		int pow = 1;
-
-		int j;
-
-		int d;
-
-		for (j = 0; j < digits - i - 1; j++)
-		{
-			pow = pow * 10;
-		}
-
-		d = ((n / pow) % 10);
-		if (n < 0)
-		{
-			d = -d;
-			if (i == 0)
-				_putchar(45);
-		}
-		_putchar(48 + d);
-	}
+	
+	if ((num / 10) > 0)
+	       
+		print_number(num / 10);
+	
+	_putchar((num % 10) + '0');
 }
