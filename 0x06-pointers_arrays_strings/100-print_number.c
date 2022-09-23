83c267003e169f63cb3b3e95e6aaebd51956d75d
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-22 18:56:59 -0700

    Advanced Task

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,46 +1,49 @@
 #include "main.h"
-
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - print integer
+ * Return: void
+ * @n: number
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	unsigned int size = n;
 
-	b = 10;
+	int digits = 0;
 
-	if (n < 10 && n >= 0)
+	int i;
+
+	if (n < 0)
+		size = -n;
+	if (n == 0)
+		digits = 1;
+
+	while (size >= 1)
 	{
-		_putchar (n + '0');
+		size = size / 10;
+		digits++;
 	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
 
-	else
+	for (i = 0; i < digits; i++)
 	{
+
+		int pow = 1;
+
+		int j;
+
+		int d;
+
+		for (j = 0; j < digits - i - 1; j++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
 		if (n < 0)
 		{
-			n = n * -1;
-			_putchar ('-');
+			d = -d;
+			if (i == 0)
+				_putchar(45);
 		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
+		_putchar(48 + d);
 	}
 }
