778f6002bef91f13ea3920320b41c588ab1df91c
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-23 17:30:45 -0700

    Task 9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,46 @@
 #include "main.h"
 
 /**
- * rot13 - Function that reverses the content of an array of integers.
+ * rot13 - rot13 encoder
+ * @str: String to be encoded
  *
- * @str: s is the array
- *
- * Return: Always 0.
+ * Return: Address of the encoed string
  */
+
 char *rot13(char *str)
 {
-	int i = 0, k;
-	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		for (k = 0; k <= 51; k++)
-		{
-			if (s[k] == str[i])
-			{
-				str[i] = s1[k];
-			}
-		}
+		str[i] = transform_2(str[i]);
+		i++;
 	}
 	return (str);
+}
+
+/**
+ * transform_2 - helper function to map a letter with its rot13 encoding
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+
+char transform_2(char x)
+{
+	char one[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char tuw[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	char replacement = x;
+
+	while (i < 52)
+	{
+		if (x == one[i])
+		{
+			replacement = two[i];
+			break;
+		}
+		i++
+	}
+	return (replacement);
 }
