a78a4680a244f649a1c51e44a8c598d3d045f65b
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-09-27 23:24:38 -0700

    Adjusted task 2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies @n bytes from the memory area pointe *           to by @src into that pointed to by @dest.
+ *_memcpy - Copies @n bytes from the memory area pointe *           to by @src into that pointed to by @dest.
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
