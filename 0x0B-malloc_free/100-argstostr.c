fb96096e020fe77653b2fefde25dc32732377510
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-10-04 17:49:04 -0700

    Adjustment of Task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,53 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
- **argstostr -  a function that concatenates all the arguments of your program.
- *@ac : int
- *@av : array
- *Return: array
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
+
+
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *argstostr - description
+ * @ac: int
+ * @av: arguments
+ * Return: string
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int x, y, i, j, l = 0, a = 0;
-	char *s;
+int i = 0, nc = 0, j = 0, cmpt = 0;
+char *s;
 
-	if (ac == 0 || av == NULL)
+if (ac == 0 || av == NULL)
 	return (NULL);
-	for (i = 0; (i < ac); i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		l++;
-		l++;
-	}
 
-	s = malloc(sizeof(char) * l + 1);
-	if (s == NULL)
+for (; i < ac; i++, nc++)
+	nc += _strlen(av[i]);
+
+s = malloc(sizeof(char) * nc + 1);
+if (s == 0)
 	return (NULL);
-	for (x = 0; x < ac ; x++)
-	{
-		for (y = 0; av[x][y] != '\0'; y++)
-		{
-			s[a] = av[x][y];
-			a++;
-		}
-		s[a++] = '\n';
-	}
-	s[a] = '\0';
-	return (s);
+
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
+
+	s[cmpt] = '\n';
+	cmpt++;
+}
+s[cmpt] = '\0';
+
+return (s);
 }
