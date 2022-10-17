bdd9b1a5d35d174408b7888d9079f2379200d6bf
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-10-16 19:04:44 -0700

    Printing the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main  This is the function to print the name followed  by a new line
+ * @argc: Counts of content
+ * @argv: The strings typed
+ * Return: 0  for success
+ */
+
+int main ( int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return 0;
+}
