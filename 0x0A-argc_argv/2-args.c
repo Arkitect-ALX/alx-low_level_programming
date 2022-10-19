c1fc62b85c6bd936a52498651760858ebdd22804
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-10-19 09:00:46 -0700

    Adjusted task to fit betty coding style

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main   - To print content within the command line by line
+ * @argc: - takes note of character count
+ * @argv: - takes note of the characters themselves
+ * Return: - 0 in success
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	return (0);
