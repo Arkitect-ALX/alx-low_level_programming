153cc3fcdff589bd3c9632140920332e27d1ba9a
Aruoture <aruoture_ivwighren@yahoo.co.uk>
2022-10-19 06:31:43 -0700

    Task 1 to print content of string in a c command

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Funtion to print the count of string
+ * @argc: The count of strin in CLI
+ * @argv: The strng content
+ * Return: 0 for success
+ *
+ */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
